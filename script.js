174194bac2175e882592323d660f05ffeac22f5b
kanaru-ssk <kanaru.ssk@gmail.com>
2024-09-12 18:03:35 +0900

    チェック機能追加

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function add() {
   const newTaskInput = document.getElementById("new-task");
-  const newLi = document.createElement("li");
-  newLi.append(newTaskInput.value);
+  if (!newTaskInput.value) return;
 
+  const checkbox = document.createElement("input"); // <input />
+  checkbox.setAttribute("type", "checkbox"); // <input type="checkbox" />
+  const span = document.createElement("span"); // <span></span>
+  span.append(newTaskInput.value); // <span>タスク</span>
+
+  const newTaskElement = document.createElement("li"); // <li></li>
+  newTaskElement.append(checkbox); // <li><input type="checkbox" /></li>
+  newTaskElement.append(span); // <li><input type="checkbox" /><span>タスク</span></li>
+
+  // index.htmlの<ul id="tasks"></ul>の中にnewTaskElementを挿入
   const tasksUl = document.getElementById("tasks");
-  tasksUl.append(newLi);
+  tasksUl.append(newTaskElement);
 
   // 入力値をクリア
   newTaskInput.value = "";
